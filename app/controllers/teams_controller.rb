b24ba1a5c5f6e0b1fdc3440ecc6e31281a060a27
Alexander Balashov <divineforest@gmail.com>
2012-10-13 19:13:45 +0400

    Cleanup Team page headings

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,16 @@
 class TeamsController < ApplicationController
+  before_filter :require_team, only: [:show]
 
   def show
+  end
+
+  private
+
+  def require_team
+    unless current_team
+      redirect_to root_url, alert: "Team required."
+      false
+    end
   end
 
 end
