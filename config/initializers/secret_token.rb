ca174b8587c1b28cc21e6cf0b517f39d89b7f274
Vladimir Strakhov <vl.strakhov@gmail.com>
2012-10-13 09:11:53 +0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhereMyMates::Application.config.secret_token = 'f052af57fcafa1caa5708167ca7b4e3e1c4e58c9c600be43696897061d5f7aef6d1b324714f0da9485544334dd1229852deb3df1e9d6c7b71a14d4aa10f8939d'
