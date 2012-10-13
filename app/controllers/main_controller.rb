7e5bce3d60be004f54706fbb7ec6015016bbf694
Alexander Balashov <divineforest@gmail.com>
2012-10-13 19:05:26 +0400

    Team on separate page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,6 @@
 class MainController < ApplicationController
 
   def index
-
   end
 
   def index2
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 WhereMyMates::Application.routes.draw do
   root :to => 'main#index'
+
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: redirect('/')
   match 'signout', to: 'sessions#destroy', as: 'signout'
@@ -9,4 +10,6 @@ WhereMyMates::Application.routes.draw do
   resource :user, :only => [] do
     post :update_geo, :on => :member
   end
+
+  resources :teams, only: [:show]
 end
