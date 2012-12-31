653724b40727f76b6709ba0bfb7f18a8f1a982d1
Mikhail Dieterle <MikDiet@gmail.com>
2012-12-31 12:45:35 +0300

    no needs to update location too fast

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,6 @@ set :job_template, nil
 ruby_path = "/home/deploy/.rbenv/versions/1.9.3-p194/bin/ruby"
 job_type :rake, "cd :path && RAILS_ENV=:environment #{ruby_path} bin/rake :task --silent :output"
 
-every 5.minutes do
+every 30.minutes do
   rake "app:location"
 end
