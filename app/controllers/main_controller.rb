2a74b7ba0c91833e7f389a3e81b818156cfc97f5
Mikhail Dieterle <MikDiet@gmail.com>
2013-04-24 12:09:30 +0300

    Error page now fixed

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,4 +4,7 @@ class MainController < ApplicationController
     @teams = Team.where('entries_count > 0').order('entries_count DESC')
   end
 
+  def error
+    render text: 'All right!'
+  end
 end
